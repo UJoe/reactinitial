7f1e2b5bd67511bff96ecaf5bb1ea50ace946847
Utri Gergely <utrig@yahoo.com>
2021-04-30 11:43:41 +0200

    still, still

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Team from "./components/Team";
 
-//still workin on others
+//still workin on others, still, still...
 
 const App = () => {
   const [teams, setTeams] = useState([
